278a56b230c1113e9e39047da5bbbd99c6d3b302
Alyza Pramudya <rahimaalyza@gmail.com>
2025-07-31 13:37:13 +0700

    Update Projects

--- components/projects.tsx
+++ components/projects.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 import Link from "next/link"
 import { cn } from "@/lib/utils"
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { Github, ExternalLink, FileIcon as FilePresentation, ChevronLeft, ChevronRight, ChevronDown, ChevronUp } from "lucide-react"
+import { Github, ExternalLink, FileIcon as FilePresentation, ChevronLeft, ChevronRight, Video } from "lucide-react"
 
 export default function Projects() {
   const projects = [
@@ -17,6 +17,7 @@ export default function Projects() {
       github: "https://github.com/pramudyalyza/DeskFairy",
       demo: "https://drive.google.com/file/d/1rOUbMn-4cTcn59OpBiH22nOb2KQ2hkjq/view?usp=drive_link",
       presentation: null,
+      app: null,
       category: "AI & Data Science",
     },
     {
@@ -24,8 +25,9 @@ export default function Projects() {
       title: "AskToAl",
       description: "AskToAl is a client-side AI chatbot for my portfolio website that provides instant answers via fine-tuned Sentence Transformers",
       image: "/asktoal.png",
-      demo: "https://pramudyalyza.vercel.app/chatbot",
+      demo: null,
       presentation: null,
+      app: "https://pramudyalyza.vercel.app/chatbot",
       category: "AI & Data Science",
     },
     {
@@ -36,6 +38,7 @@ export default function Projects() {
       github: "https://github.com/pramudyalyza/dearCSV",
       demo: "https://drive.google.com/file/d/1gqXAA2YDuWBHqhLtgbmy_GR8qArOlZFH/view?usp=drive_link",
       presentation: null,
+      app: null,
       category: "AI & Data Science",
     },
     {
@@ -46,6 +49,7 @@ export default function Projects() {
       github: "https://github.com/pramudyalyza/askMeGirl",
       demo: "https://drive.google.com/file/d/1PJnp4e4Mzx_USqSPz__G3F-DETh8Cvx6/view?usp=drive_link",
       presentation: null,
+      app: null,
       category: "AI & Data Science",
     },
     {
@@ -56,6 +60,7 @@ export default function Projects() {
       github: "https://github.com/pramudyalyza/prompt-and-prejudice",
       demo: "https://drive.google.com/file/d/1DOFzRS4z89vLvQ-4CyoTVETf0SkVShYI/view?usp=drive_link",
       presentation: null,
+      app: null,
       category: "AI & Data Science",
     },
     {
@@ -64,8 +69,9 @@ export default function Projects() {
       description: "A web app that predicts dog breeds from images using a fine-tuned AlexNet. Upload, use camera, or URL. Supports 10 predefined breeds.",
       image: "/DogBreedClassifierz.jpg",
       github: "https://github.com/pramudyalyza/dogBreedClassifierz",
-      demo: "http://dogbreedclassifierz.streamlit.app/",
+      demo: null,
       presentation: "https://drive.google.com/file/d/1jZEHkyWy7iQkgJOZ4ZLtDZ_HLE9ZF5EM/view?usp=drive_link",
+      app: "http://dogbreedclassifierz.streamlit.app/",
       category: "AI & Data Science",
     },
     {
@@ -74,138 +80,163 @@ export default function Projects() {
       description: "A sentiment tracker for Indonesia’s new capital using ML on YouTube comments. View daily trends or test your own text.",
       image: "/iknSentiment.jpg",
       github: "http://github.com/pramudyalyza/IKN-SentimentApp",
-      demo: "http://iknsentimentapp.streamlit.app/",
+      demo: null,
       presentation: "https://drive.google.com/drive/folders/1YuHREcAEAMtEOJmNJYfL5YIjLHGKKHTw?usp=drive_link",
+      app: "http://iknsentimentapp.streamlit.app/",
       category: "AI & Data Science",
     },
     {
       id: 8,
+      title: "Sugar Signal",
+      description: "A simple full-stack web app to learn how to serve a diabetes prediction ML model, with the backend on Railway and the frontend on Vercel.",
+      image: "/sugarSignal.jpg",
+      github: "https://github.com/pramudyalyza/SugarSignal",
+      demo: null,
+      presentation: null,
+      app: "https://sugar-signal.vercel.app/",
+      category: "AI & Data Science",
+    },
+    {
+      id: 9,
       title: "RPA Loan Request",
       description: "UiPath automation script that use REFramework to manage daily loan requests via dispatcher-performer setup, including handling queue handling, form, email, and file.",
       image: "/loanRequest.jpg",
       github: "https://github.com/pramudyalyza/RPA-Loan-Request",
       demo: "https://drive.google.com/file/d/1fn9Q9gfAELSpt8k_pdhhuzN61Kf5Rg0a/view?usp=drive_link",
       presentation: null,
+      app: null,
       category: "RPA",
     },
     {
-      id: 9,
+      id: 10,
       title: "RPA Nationality API",
       description: "UiPath automation script to predict top 3 nationalities for a list of names and records the results in Excel using the Nationalize.io API.",
       image: "/nationalityAPI.jpg",
       github: "https://github.com/pramudyalyza/RPA-Nationality-API",
       demo: "https://drive.google.com/file/d/1f8OjtdwjklRLXmOFWPnAR9A3Wm7MJADw/view?usp=drive_link",
       presentation: null,
+      app: null,
       category: "RPA",
     },
     {
-      id: 10,
+      id: 11,
       title: "RPA Invoice RegEx",
       description: "UiPath automation script to extract invoice number, customer ID, total amount, and due date from PDF files using regular expressions.",
       image: "/invoiceRegex.jpg",
       github: "https://github.com/pramudyalyza/RPA-Invoice-Regex",
       demo: "https://drive.google.com/file/d/1tYP7RAL258dDHqIEU1G1_fHL7l7qjpnc/view?usp=drive_link",
       presentation: null,
+      app: null,
       category: "RPA",
     },
     {
-      id: 11,
+      id: 12,
       title: "RPA Web Scrapping",
       description: "UiPath automation script to scrape population data by age group and gender from the BPS website (2018 till 2025) and export it to CSV files.",
       image: "/webScrap.jpg",
       github: "https://github.com/pramudyalyza/RPA-Web-Scrapping",
       demo: "https://drive.google.com/file/d/1TZuQD32gEhykuu0Phw1sa_wQ2nxKPCxd/view?usp=drive_link",
       presentation: null,
+      app: null,
       category: "RPA",
     },
     {
-      id: 12,
+      id: 13,
       title: "RPA Excel and Email",
       description: "UiPath automation script to update monthly master data with the latest MTD figures, generate pivot reports, and email them to stakeholders.",
       image: "/excelEmail.jpg",
       github: "https://github.com/pramudyalyza/RPA-Excel-and-Email",
       demo: "https://drive.google.com/file/d/1BeaIU4jJWW19_N2cWRrR2dyiA3XW5wl2/view?usp=drive_link",
       presentation: null,
+      app: null,
       category: "RPA",
     },
     {
-      id: 13,
+      id: 14,
       title: "Frezz : Fruit Freshness Detector",
       description: "Frezz detects fruit freshness to assist farmers and supermarket staff in quality control, focusing on apples, bananas, and oranges.",
       image: "/frezz.png",
       github: "https://github.com/pramudyalyza/frezz",
       demo: null,
       presentation: "https://drive.google.com/file/d/1vHnm6AyTlvJx61R6J-uQyzNDE3RgtBoC/view?usp=drive_link",
+      app: null,
       category: "AI & Data Science",
     },
     {
-      id: 14,
+      id: 15,
       title: "Covid-19 in US: Weather & Socioeconomic Factors",
       description: "Analyzed how weather and socioeconomic factors affect U.S. COVID-19 cases, supported by an interactive dashboard to visualize key insights.",
       image: "/covid.png",
       github: "https://github.com/pramudyalyza/COVID19_in_US_Weather_Socioeconomic_Factors",
       demo: "https://bit.ly/AlyzaProject02-Tableau",
       presentation: "https://drive.google.com/file/d/1P_2zLRi4VjabM4zxgM5g5A-pAzT90SUu/view?usp=drive_link",
+      app: null,
       category: "AI & Data Science",
     },
     {
-      id: 15,
+      id: 16,
       title: "Urban Visual Pollutants Detection",
       description: "Built a system to detect and classify urban visual pollutants in images, using a dataset featuring graffiti, faded signage, and more.",
       image: "/urban.png",
       github: "https://github.com/pramudyalyza/Urban-Visual-Pollutants-Detection/tree/main",
       demo: null,
       presentation: "https://drive.google.com/file/d/1RZAqaMlVHX0D0D5Jl9Htv4Yw2Iu71oKL/view?usp=drive_link",
+      app: null,
       category: "AI & Data Science",
     },
     {
-      id: 16,
+      id: 17,
       title: "WHO: Life Expectancy Analysis",
       description: "Explored Kaggle’s Life Expectancy dataset to identify key factors and built a predictive model using Random Forest.",
       image: "/who.png",
       github: "https://github.com/pramudyalyza/WHO-LifeExpectancyAnalysis",
       demo: "https://bit.ly/AlyzaProject04-Tableau",
       presentation: "https://drive.google.com/file/d/1RqLpIodPB_9RtBsRE8nBtoPJvwVu_RHL/view?usp=drive_link",
+      app: null,
       category: "AI & Data Science",
     },
     {
-      id: 17,
+      id: 18,
       title: "News Category Classification",
       description: "Classifies Indonesian news articles using scraped data from Kompas, Pikiran Rakyat, Tribunnews, and Merdeka.",
       image: "/news.png",
       github: "https://github.com/pramudyalyza/News-Category-Classification/tree/main",
       demo: null,
       presentation: "https://drive.google.com/file/d/1PpFHvyuq1-gPU9txAjYlxtERTlZZ-AF3/view?usp=drive_link",
+      app: null,
       category: "AI & Data Science",
     },
     {
-      id: 18,
+      id: 19,
       title: "Jakarta Air Quality Classification",
       description: "A model forecasting Jakarta’s air pollution to support government analysis of air quality factors.",
       image: "/airQuality.png",
       github: "https://github.com/pramudyalyza/Jakarta-Air-Quality-Classification",
       demo: null,
       presentation: "https://drive.google.com/file/d/1rCAdiJH55q8YvCvJYU86I8rAkAvTNzVz/view?usp=drive_link",
+      app: null,
       category: "AI & Data Science",
     },
     {
-      id: 19,
+      id: 20,
       title: "Diabetes Classification & Regression",
       description: "A model to classify diabetes outcome and predict pedigree function using key health indicators like glucose and pregnancies.",
       image: "/diabetes.png",
       github: "https://github.com/pramudyalyza/Diabetes-Classification-Regression",
       demo: null,
       presentation: "https://drive.google.com/file/d/1rgQS1SwsuWpg_PU-A28jPQK9JGEGWEAn/view?usp=drive_link",
+      app: null,
       category: "AI & Data Science",
     },
     {
-      id: 20,
+      id: 21,
       title: "Telco Customer Churn Prediction",
       description: "Developed a predictive model to identify Telco customers likely to churn, helping reduce customer loss.",
       image: "/telco.png",
       github: "https://github.com/pramudyalyza/TelcoCustomerChurn_R",
       demo: null,
       presentation: "https://drive.google.com/file/d/1Z-WT5ykqrgV1VcgHNYvhcPJPdelSl8CY/view?usp=drive_link",
+      app: null,
       category: "AI & Data Science",
     },
   ]
@@ -374,6 +405,17 @@ export default function Projects() {
                         target="_blank"
                         rel="noopener noreferrer"
                         aria-label={`Live demo for ${project.title}`}
+                      >
+                        <Video className="h-5 w-5" />
+                      </Link>
+                    )}
+                    {project.app && (
+                      <Link
+                        href={project.app}
+                        className="w-8 h-8 flex items-center justify-center rounded-full bg-primary text-muted shadow hover:shadow-md transition"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label={`Application for ${project.title}`}
                       >
                         <ExternalLink className="h-5 w-5" />
                       </Link>
